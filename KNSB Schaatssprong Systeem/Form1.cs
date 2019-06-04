70fcfb48630b5fce77d8b83ecdb946b7c2c2b8d3
BartHiemstra <bart----@hotmail.com>
2019-06-04 14:46:44 +0200

    First commit

---
+++ KNSB Schaatssprong Systeem/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KNSB_Schaatssprong_Systeem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+    }
+}
